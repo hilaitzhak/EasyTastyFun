b866d342f075809cbe1cc6a8577bc6e0569dbe3e
Hila Itzhak <hila87219@gmail.com>
2025-07-04 14:58:44 +0300

    fix cors errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -31,9 +31,9 @@ export class AppServer {
 
     private setMiddlewares() {
         this.app.use(cors({
-            origin: 'http://localhost:5173',
+            // origin: 'http://localhost:5173',
             credentials: true,
-            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+            // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
           }));
         this.app.use(express.json({ limit: '50mb' }));
         this.app.use((req, res, next) => {
